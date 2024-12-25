9e00c88c43f19410a46b4ae9d31c09284f5c2afa
william-wtr92 <wwautrin@hotmail.fr>
2024-12-25 17:47:40 +0100

    chore: shadcn ui package

--- apps/client/tailwind.config.ts
+++ apps/client/tailwind.config.ts
@@ -2,8 +2,8 @@ import type { Config } from "tailwindcss"
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "../../packages/ui/src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/web/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
---
+++ packages/ui/eslint.config.mjs
@@ -0,0 +1,17 @@
+import { FlatCompat } from "@eslint/eslintrc"
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: { parserOptions: { ecmaVersion: "latest" } },
+})
+
+const eslintConfig = [
+  ...compat.config({ extends: ["@lootopia/eslint-config-client"] }),
+]
+
+export default eslintConfig
