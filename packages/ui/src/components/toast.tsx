bf7436c86c60973a7ba0cfdf28c48c8e93601ce9
Scalpal <90528753+Scalpal@users.noreply.github.com>
2025-02-20 09:48:21 +0100

    fix: navbar buttons and toast z-index fix

--- packages/ui/src/components/toast.tsx
+++ packages/ui/src/components/toast.tsx
@@ -16,7 +16,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed right-0 top-0 z-[100] flex max-h-screen w-full flex-col p-4 md:max-w-[420px]",
+      "fixed right-0 top-0 z-[9999] flex max-h-screen w-full flex-col p-4 md:max-w-[420px]",
       className
     )}
     {...props}
