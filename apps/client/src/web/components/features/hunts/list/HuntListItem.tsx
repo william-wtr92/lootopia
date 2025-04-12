c0692d77455709fb3cff51ebb0f3401b78f2ca94
william-wtr92 <wwautrin@hotmail.fr>
2025-04-12 11:25:05 +0200

    fix: dead code

--- apps/client/src/web/components/features/hunts/list/HuntListItem.tsx
+++ apps/client/src/web/components/features/hunts/list/HuntListItem.tsx
@@ -244,21 +244,3 @@ const hiddenRewardsListVariant = {
     },
   },
 }
-
-// const rewardItemVariant = {
-//   initial: { opacity: 0, scale: 0 },
-//   enter: {
-//     opacity: 1,
-//     scale: 1,
-//     transition: {
-//       duration: 0.3,
-//     },
-//   },
-//   exit: {
-//     opacity: 0,
-//     scale: 0,
-//     transition: {
-//       duration: 0.3,
-//     },
-//   },
-// }
